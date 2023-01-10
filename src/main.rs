e5d35dfb5666568fd715c5851a093a97b70b2d80
Manuel Gonzalez <mgonzalez4648@gmail.com>
2023-01-10 17:31:04 +0100

    Default journal path and error pretty printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
-mod cli;
+use anyhow::anyhow;
 use structopt::StructOpt;
+mod cli;
+mod tasks;
+use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
 
-fn main() {
-    cli::CommandLineArgs::from_args();
+const DEFAULT_JOURNAL_PATH: &str = "journal.json";
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(DEFAULT_JOURNAL_PATH);
+        path
+    })
+}
+
+fn main() -> anyhow::Result<()> {
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file.
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
+
+    // Perform the action.
+    match action {
+        Add { task } => tasks::add_task(journal_file, tasks::Task::new(task)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+
+    Ok(())
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -3,6 +3,8 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::fmt;
 
+const DEFAULT_TIME_FORMAT: &str = "%d-%m-%Y %H:%M:%S";
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
@@ -11,9 +13,16 @@ pub struct Task {
     pub created_at: DateTime<Utc>,
 }
 
+impl Task {
+    pub fn new(text: String) -> Task {
+        let created_at: DateTime<Utc> = Utc::now();
+        Task { text, created_at }
+    }
+}
+
 impl fmt::Display for Task {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        let created_at = self.created_at.with_timezone(&Local).format(DEFAULT_TIME_FORMAT);
         // We indicate it is padded by 50 spaces
         write!(f, "{:<50} [{}]", self.text, created_at)
     }
@@ -48,7 +57,7 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     Ok(())
 }
 
-use std::io::{Error, ErrorKind, Result, Seek, SeekFrom}; // Include the `Error` type.
+use std::io::{Error, ErrorKind}; // Include the `Error` type.
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
     // Open the file.
@@ -83,7 +92,7 @@ pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
         println!("Task list is empty!");
     } else {
         for i in 0..tasks.len() {
-            println!("{}: {}", i + 1, task);
+            println!("{}: {}", i + 1, tasks.get(i).unwrap());
         }
     }
 
