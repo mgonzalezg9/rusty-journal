a615300d6f51790c414ab9dfb73845d7f44737a2
Manuel Gonzalez <mgonzalez4648@gmail.com>
2023-01-10 13:51:35 +0100

    Parsed cli args using structopt

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    /// Write tasks to journal file
+    Add {
+        #[structopt()]
+        task: String,
+    },
+    /// Remove one task
+    Done {
+        #[structopt()]
+        position: usize,
+    },
+    /// List all tasks written
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Rusty Journal",
+    about = "A command line to-do app written in Rust"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+
+    /// Use a different journal file.
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
